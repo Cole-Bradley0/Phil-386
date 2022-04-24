c284a2be4733694df1458e7ecee1de76de787adb
Cole-Bradley0 <69804583+Cole-Bradley0@users.noreply.github.com>
2022-04-24 16:17:13 -0500

    Made a raycast that indicates when the player can interact with the computer

---
+++ Phil 386 Game/Assets/StarterAssets/Coles Scripts and Stuff/PlayerRaycast.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public LayerMask layers;
+    public GameObject interactionText;
+    public bool lookingAtComputer;
+    public float raycastLength = 5f;
+    
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, raycastLength, layers))
+        {
+            if(hit.transform.tag == "Computer")
+            {
+                lookingAtComputer = true;
+            }
+            else
+            {
+                lookingAtComputer = false;
+            }
+        }
+        else
+        {
+            lookingAtComputer = false;
+        }
+    }
+}
