68eb82e3e9265fce8cad096ed15ef87f17e9e391
Cole-Bradley0 <69804583+Cole-Bradley0@users.noreply.github.com>
2022-05-03 13:38:01 -0500

    TV Service Script

---
+++ Phil 386 Game/Assets/StarterAssets/Coles Scripts and Stuff/TV_Service.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TV_Service : MonoBehaviour
+{
+    public VideoPlayer player;
+    public VideoClip[] clips;
+
+    public bool cooldown = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!player.isPlaying && !cooldown)
+        {
+            PlayRandomClip();
+        }
+    }
+
+
+
+    [ContextMenu("play")]
+    void PlayRandomClip()
+    {
+        cooldown = true;
+        player.clip = clips[Random.Range(0, clips.Length)];
+        player.Play();
+        StartCoroutine(CooldownTimer());
+    }
+
+    IEnumerator CooldownTimer()
+    {
+        yield return new WaitForSeconds(5f);
+        cooldown = false;
+    }
+}
