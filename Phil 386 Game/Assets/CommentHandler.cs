8d17ca06db44c72edb95dd69bdc169a863ad8fa5
reagan00010100 <104416223+reagan00010100@users.noreply.github.com>
2022-05-05 12:51:34 -0500

    foidsa
    fdsaoijoi

--- Phil 386 Game/Assets/CommentHandler.cs
+++ Phil 386 Game/Assets/CommentHandler.cs
@@ -19,7 +19,7 @@ public class CommentHandler : MonoBehaviour
     }
 
     private void CreateComment(){
-        string[] comments = {"Test","Lol","Pop"};
+        string[] comments = {"5 out 5 would buy again This changed my life and I feel like I can go on again", "I would recommend this to all my friends","This is the program that would save society from the edge of collapse","This is a useful program that I'll use everyday","Well this works... or at least well enough","Needs more orange","I've used better but I've seen worse","Wai this isn't a review for ice cream", "Somebody once told me the world was going to roll me...","0 out of 5 this osftware sucks","I'd have rather stub my toe than buy this again","Was this created over the course of a night","My children could make a better app About as useful as a square wheel"};
             
             comment.text = comments[Random.Range(0,comments.Length-1)];
     }
